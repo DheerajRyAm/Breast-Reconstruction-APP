b9480b75ecae0991ec062bc494b779ca1c458333
Sophia Li <sophia1616li@gmail.com>
2024-11-03 20:21:58 -0500

    Area calculation with model

---
+++ ai/Segmentation.py
@@ -0,0 +1,43 @@
+# pip install inference supervision
+# Have cv2 and numpy installed too
+
+# python ai/Segmentation.py
+
+from inference import get_model
+import supervision as sv
+import cv2
+import numpy as np
+import os
+
+os.environ['ROBOFLOW_API_KEY'] = 'sDVzESlGa38E7jn9kI6I'
+
+# define the image to use for inference
+image_file = "ai/Pt 1 LCC.jpg"
+image = cv2.imread(image_file)
+
+# load pre-trained yolov8n model
+model = get_model(model_id="breast-segmentation-dxcby/3", api_key=os.getenv("ROBOFLOW_API_KEY"))
+
+# run inference on our chosen image
+results = model.infer(image)
+
+total_area = 0
+contour_image = np.zeros(image.shape, dtype=np.uint8)
+
+for prediction in results[0].predictions:
+    points = prediction.points
+    contour_points = np.array([[point.x, point.y] for point in points], dtype=np.int32)
+    
+    # Calculate the area of the contour
+    area = cv2.contourArea(contour_points)
+    total_area += area
+
+    cv2.drawContours(contour_image, [contour_points], contourIdx=-1, color=(255, 255, 255), thickness=2)
+
+print(f"Total Area of Segmented Image: {total_area} pixels")
+# all images are 8.53x 11.09 In (2560 x 3328 pixels at 300 ppi).
+area_scaled = total_area / 90000
+print(f"Total Area of Segmented Image: {area_scaled} in^2")
+
+# Display the contour image
+cv2.imwrite("segmented_contours.jpg", contour_image)
